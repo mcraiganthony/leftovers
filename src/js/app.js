8550172a95ba85dec11e26d054445cd3def5e062
Craig Anthony <mcraiganthony@gmail.com>
2015-12-09 16:09:36 -0500

    Craig - start

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,22 @@ function ready(fn) {
 function init() {
 	console.log ('app js loaded');
 	boomsvgloader.load('icons/sprite.svg');
+	movie();
 }
+
+
+function movie() {
+
+
+var tl = new TimelineMax({repeat:10, repeatDelay:4, });
+tl.to([".scene"], 6, {scale: 1.1});
+
+}
+
+
+
+
+
+
 
 ready(init);
