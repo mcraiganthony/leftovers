387cce11459df5515f1d5cd793326577a94ab7e2
Craig Anthony <mcraiganthony@gmail.com>
2015-12-11 10:18:49 -0500

    Craig - timeline adjustments

--- src/js/app.js
+++ src/js/app.js
@@ -19,20 +19,17 @@ function init() {
 
 function movie() {
 
-	var tl = new TimelineMax({repeat:4, repeatDelay:0, });
+	var tl = new TimelineMax({repeat:4, repeatDelay:0});
 	tl.set('.scene1', {visibility: 'visible'}); 
-	tl.to([".scene1"], 3, {scale: 1.03});
-	tl.set('.scene3', {delay: -0.25, visibility: 'visible'});
-	tl.to([".scene3"], 4, {delay: -0.25, scale: 1.05});
-	tl.set('.scene4', {delay: -0.25, visibility: 'visible'});
-	tl.to([".scene4"], 4, {delay: -0.25, scale: 1.05});
-	tl.set('.scene8', {delay: -0.25, visibility: 'visible'});
-	tl.to([".scene8"], 3, {delay: -0.25, scale: 1.03});
+	tl.to([".scene1"], 3, {scale: 1.03, ease: Linear.easeNone });
+	tl.set('.scene3', {delay: -1, visibility: 'visible'});
+	tl.to([".scene3"], 4, {delay: -1, scale: 1.05, ease: Linear.easeNone});
+	tl.set('.scene4', {delay: -1, visibility: 'visible'});
+	tl.to([".scene4"], 4, {delay: -1, scale: 1.05, ease: Linear.easeNone});
+	tl.set('.scene8', {delay: -1, visibility: 'visible'});
+	tl.to([".scene8"], 3, {delay: -1, scale: 1.03, ease: Linear.easeNone});
 
 }
-
-
-
 
 
 
